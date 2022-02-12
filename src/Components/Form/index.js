9cc5b053279d3bd4d07d4c79094fa200714977c5
atila-tokyo <atilamanager@gmail.com>
2022-02-12 21:57:29 +0900

    Adding method create to api

--- src/Components/Form/index.js
+++ src/Components/Form/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
-export const Form = () => {
+export const Form = ({ userInput, onFormChange, onFormSubmit }) => {
+    
+    const handleChange = (event) => {
+        onFormChange(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onFormSubmit();
+    }
     return (
         <div className='container'>
-            <form>
-                <input type='text' required></input>
-                <input type='submit'></input>
+            <form onSubmit={handleSubmit}>
+                <input type='text' required value={userInput} onChange={handleChange} />
+                <input type='submit' />
             </form>
         </div>
-    )
+    );
 }
