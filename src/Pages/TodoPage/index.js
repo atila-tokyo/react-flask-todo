25a0970c74caa5cdc8da6e1739e3e62fb69cba07
atila-tokyo <atilamanager@gmail.com>
2022-02-12 16:39:28 +0900

    Adding basic react structure

---
+++ src/Pages/TodoPage/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Card } from '../../Components/Card';
+
+export const TodoPage = () => {
+    return (
+        <>
+            <Card />
+        </>
+    )
+}
+
+
