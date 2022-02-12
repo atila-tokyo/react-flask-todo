e1ff0bd277c82a2014d43067b56d3f745d83a1b7
atila-tokyo <atilamanager@gmail.com>
2022-02-12 19:53:53 +0900

    Adding form component

---
+++ src/Components/Form/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Form = () => {
+    return (
+        <div className='container'>
+            <form>
+                <input type='text' required></input>
+                <input type='submit'></input>
+            </form>
+        </div>
+    )
+}
