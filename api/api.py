42f4a41b4c0f738996191eb41a130b4b86851beb
atila-tokyo <atilamanager@gmail.com>
2022-02-12 19:46:59 +0900

    Adding untracked files

--- api/api.py
+++ api/api.py
@@ -1,14 +1,28 @@
 # Import Flask class
-from flask import Flask
- 
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 # Create Flask object
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///persistence.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+# app.config['SECRET_KEY']
 
-app.config['SECRET_KEY']
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
 
-@app.route('/', methods=['GET', 'POST'])
+    def __str__(self):
+        return f'{self.id} {self.content}'
+
+def serializer(todo):
+    return {
+        'id': todo.id,
+        'content': todo.content
+    }
+@app.route('/api', methods=['GET'])
 def index():
-    return 'test'
+    return jsonify([*map(serializer, Todo.query.all())])
 
 
 if __name__ == '__main__':
--- src/Pages/TodoPage/index.js
+++ src/Pages/TodoPage/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card } from '../../Components/Card';
 
 export const TodoPage = () => {
+
+    const [todo, setTodo] = useState([]);
+
+    useEffect(() => {
+        fetch('/api')
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                }
+            })
+            .then(data => setTodo(data))
+    }, [])
     return (
         <>
-            <Card />
+            <Card todoList={ todo }/>
         </>
     )
 }
